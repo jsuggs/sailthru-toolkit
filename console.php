c4c6c28a5a3391699414ab284dee7e5a1ca676a6
Jonathon Suggs <jsuggs@murmp.com>
2012-12-21 13:30:37 -0600

    Export the scheduled sends

--- console.php
+++ console.php
@@ -10,6 +10,7 @@ $console = new Symfony\Component\Console\Application('SailThru Awesomeness', '0.
 // Add the console commands here
 $console->add(new SailThru\Command\CopyTemplateCommand($app));
 $console->add(new SailThru\Command\DownloadTemplatesCommand($app));
+$console->add(new SailThru\Command\ExportScheduledSendsCommand($app));
 $console->add(new SailThru\Command\SearchTemplatesCommand($app));
 $console->add(new SailThru\Command\SendEmailCommand($app));
 $console->add(new SailThru\Command\TemplateStatisticsReportCommand($app));
---
+++ src/SailThru/Command/ExportScheduledSendsCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SailThru\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportScheduledSendsCommand extends AbstractSailThruCommand
+{
+    private $client;
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this
+            ->setDescription('Export the scheduled sends')
+            ->addArgument('env',   InputArgument::REQUIRED, 'The env to update')
+            ->addArgument('email', InputArgument::OPTIONAL, 'The email to send the report to')
+        ;
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        parent::initialize($input, $output);
+
+        $this->client = $this->getSailThruClient($input->getArgument('env'));
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf(
+            'Exporting the scheduled sends for env: %s. Sending to "%s"',
+            $input->getArgument('env'),
+            $input->getArgument('email')
+        ));
+
+        $options = array(
+            'job' => 'export_scheduled_sends',
+        );
+
+        if ($input->getArgument('email')) {
+            $options['report_email'] = $input->getArgument('email');
+        }
+
+        $response = $this->client->apiPost('job', $options);
+        $this->displayResponse($response);
+    }
+
+    public function getCommandName()
+    {
+        return 'export-scheduled-sends';
+    }
+}
